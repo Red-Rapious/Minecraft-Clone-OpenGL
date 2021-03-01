389d6b32d73170c0d7eee1b6c8cb22f8da8e0d3f
Red Rapious <red.rapious@gmail.com>
2021-03-01 20:22:39 +0100

    Adding some externals libraries to the project: GLFW, GLEW, GLM, stb_image

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
-int main()
+int main(void)
 {
-	std::cout << "Hello World" << std::endl;
-	std::cin.get();
-	return 0;
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Initialising GLEW */
+    if (glewInit() != GLEW_OK)
+        std::cout << "[Initialisation error] Unable to initialise GLEW" << std::endl;
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
