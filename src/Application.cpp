32e14665fce16f720d70ee82482338b19ad2b2fa
Red Rapious <red.rapious@gmail.com>
2021-03-01 21:53:35 +0100

    Polished up a bit before starting to imlement some graphics

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,44 +1,78 @@
-#include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
+
+#include "Renderer.h"
+#include "IndexBuffer.h"
+#include "VertexBuffer.h"
+#include "VertexArray.h"
+#include "VertexBufferLayout.h"
+#include "Shader.h"
 
 int main(void)
 {
     GLFWwindow* window;
 
-    /* Initialize the library */
+    /* Initialize GLFW */
     if (!glfwInit())
+    {
+        std::cout << "[Initialisation error] Unable to initialise GLFW" << std::endl;
+        ASSERT(false);
         return -1;
+    }
+        
+
+    /* Setting profile to CORE to use vertex arrays objects */
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(1024, 768, "Hello World", NULL, NULL); // 4:3
 
     if (!window)
     {
+        std::cout << "[Initialisation error] Unable to create a window" << std::endl;
         glfwTerminate();
+        ASSERT(false);
         return -1;
     }
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    /* Set the swap interval to make the render smooth */
+    glfwSwapInterval(1);
+
     /* Initialising GLEW */
     if (glewInit() != GLEW_OK)
-        std::cout << "[Initialisation error] Unable to initialise GLEW" << std::endl;
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
+        std::cout << "[Initialisation error] Unable to initialise GLEW" << std::endl;
+        ASSERT(false);
+        return -1;
     }
+        
 
-    glfwTerminate();
+
+
+    {
+        /* Loop until the user closes the window */
+        while (!glfwWindowShouldClose(window))
+        {
+            /* Render here */
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            /* Swap front and back buffers */
+            glfwSwapBuffers(window);
+
+            /* Poll for and process events */
+            glfwPollEvents();
+        }
+
+        glfwTerminate();
+    
+    
+    }
     return 0;
 }
