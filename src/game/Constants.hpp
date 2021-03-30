45e32113181d093022be8f4dc3e8f8f4e944f4d5
Red Rapious <red.rapious@gmail.com>
2021-03-30 18:39:28 +0200

    Deleted useless constants

--- src/game/Constants.hpp
+++ src/game/Constants.hpp
@@ -3,15 +3,8 @@
 constexpr int         CHUNK_X_BLOCK_COUNT = 16;
 constexpr int         CHUNK_Y_BLOCK_COUNT = 256;
 constexpr int         CHUNK_Z_BLOCK_COUNT = 16;
-constexpr float       BLOCK_LENGTH = 1.f;
 
-constexpr int         TEXTURE_SIDE_LENGTH = 16; // in pixels
-constexpr int		  BLOCK_TYPES_TEXTURES_NUMBER = 12;
+constexpr int		  BLOCK_TYPES_TEXTURES_NUMBER = 12; // number of different block types in the texture
 
 constexpr int RENDER_DISTANCE = 4; // in chunks
-								    // the surface of the area renderer is ((RENDER_DISTANCE*2)+1)**2
-
-// Meta constants
-constexpr float CHUNK_X_LENGTH = CHUNK_X_BLOCK_COUNT * BLOCK_LENGTH;
-constexpr float CHUNK_Y_LENGTH = CHUNK_Y_BLOCK_COUNT * BLOCK_LENGTH;
-constexpr float CHUNK_Z_LENGTH = CHUNK_Z_BLOCK_COUNT * BLOCK_LENGTH;
+								    // the surface of the area renderer is maybe ((RENDER_DISTANCE*2)+1)**2
